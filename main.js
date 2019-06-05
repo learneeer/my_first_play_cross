2056509ddc08085287d62300922a8ebdda7e25df
learneeer <learneeer.9@gmail.com>
2019-06-05 14:40:50 +0200

    nie działa innerHTML - tylko przy 5

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
-var empty1 = document.getElementsByClassName("empty")[0];
-var empty2 = document.getElementsByClassName("empty")[1];
-var empty3 = document.getElementsByClassName("empty")[2];
-var empty4 = document.getElementsByClassName("empty")[3];
-var empty5 = document.getElementsByClassName("empty")[4];
-var empty6 = document.getElementsByClassName("empty")[5];
-var empty7 = document.getElementsByClassName("empty")[6];
-var empty8 = document.getElementsByClassName("empty")[7];
-var empty9 = document.getElementsByClassName("empty")[8];
-
 var mark = "x";
+var f;
 
 var change = function(x) {
     if (x.innerHTML === "") {
@@ -17,85 +8,51 @@ var change = function(x) {
     }
 }
 
-var check = function(x, y, z) {
-    if (x.innerHTML === y.innerHTML && x.innerHTML === z.innerHTML) {
-        x.style.background = "green";
-        y.style.background = "green";
-        z.style.background = "green";
-        setTimeout((function() {
-            window.location.reload();
-        }), 2000);
+
+
+var field = function(x, y) {
+
+    var tab1 = [];
+
+
+    var id1 = 3 * y + x + 1;
+    var id2 = 3 * (y - 1) + (x - 1) + 1;
+    var id3 = 3 * y + (x - 1) + 1;
+    var id4 = 3 * (y + 1) + (x - 1) + 1;
+    var id5 = 3 * (y - 1) + x + 1;
+    var id6 = 3 * (y + 1) + x + 1;
+    var id7 = 3 * (y - 1) + (x + 1) + 1;
+    var id8 = 3 * y + (x + 1) + 1;
+    var id9 = 3 * (y + 1) + (x + 1) + 1;
+
+    var tab = [id1, id2, id3, id4, id5, id6, id7, id8, id9];
+
+    for (i = 0; i < 9; i++) {
+        if (tab[i] > 0) { tab1.push(tab[i]); }
+
     }
+
+    console.log(tab);
+    console.log(tab1);
+
+
+
+    var f1 = document.getElementById("id" + id1).innerHTML;
+    var f2 = document.getElementById("id" + id2).innerHTML;
+    var f3 = document.getElementById("id" + id3).innerHTML;
+    var f4 = document.getElementById("id" + id4).innerHTML;
+    var f5 = document.getElementById("id" + id5).innerHTML;
+    var f6 = document.getElementById("id" + id6).innerHTML;
+    var f7 = document.getElementById("id" + id7).innerHTML;
+    var f8 = document.getElementById("id" + id8).innerHTML;
+    var f9 = document.getElementById("id" + id9).innerHTML;
+
+
+
+    for (i = 0; i < tab1.length; i++) {
+        var f = document.getElementById("id" + tab1[i]).innerHTML;
+        console.log(f);
+    }
+
+
 }
-
-
-
-var change1 = function() {
-    change(this);
-    check(empty1, empty2, empty3);
-    check(empty1, empty4, empty7);
-    check(empty1, empty5, empty9);
-}
-
-var change2 = function() {
-    change(this);
-    check(empty1, empty2, empty3);
-    check(empty2, empty5, empty8);
-}
-
-var change3 = function() {
-    change(this);
-    check(empty1, empty2, empty3);
-    check(empty3, empty5, empty7);
-    check(empty3, empty6, empty9);
-}
-
-var change4 = function() {
-    change(this);
-    check(empty4, empty5, empty6);
-    check(empty1, empty4, empty7);
-}
-
-var change5 = function() {
-    change(this);
-    check(empty2, empty5, empty8);
-    check(empty4, empty5, empty6);
-    check(empty7, empty5, empty3);
-    check(empty1, empty5, empty9);
-}
-
-var change6 = function() {
-    change(this);
-    check(empty3, empty6, empty9);
-    check(empty4, empty5, empty6);
-}
-
-var change7 = function() {
-    change(this);
-    check(empty1, empty4, empty7);
-    check(empty7, empty8, empty9);
-    check(empty7, empty5, empty3);
-}
-
-var change8 = function() {
-    change(this);
-    check(empty7, empty8, empty9);
-    check(empty2, empty5, empty8);
-}
-
-var change9 = function() {
-    change(this);
-    check(empty3, empty6, empty9);
-    check(empty7, empty8, empty9);
-    check(empty1, empty5, empty9);
-}
-
-empty1.addEventListener("click", change1);
-empty2.addEventListener("click", change2);
-empty3.addEventListener("click", change3);
-empty4.addEventListener("click", change4);
-empty5.addEventListener("click", change5);
-empty6.addEventListener("click", change6);
-empty7.addEventListener("click", change7);
-empty8.addEventListener("click", change8);
-empty9.addEventListener("click", change9);
