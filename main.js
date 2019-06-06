fe40d0bf7b177b3cded3e14f8dc54d52b859b8ea
learneeer <learneeer.9@gmail.com>
2019-06-06 15:05:38 +0200

    basic_version3

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 var mark = "x";
-var f;
+var f1;
+var f2;
+var f3;
+var f4;
+var f5;
+var f6;
+var f7;
+var f8;
+var f9;
 
 var change = function(x) {
     if (x.innerHTML === "") {
@@ -10,38 +18,129 @@ var change = function(x) {
 
 
 
-var field = function(x, y) {
+var check = function(x, y) {
 
-    var tab1 = [];
+    var check1 = function(x, y) {
+        var row = x - 1;
+        var col = y - 1;
+        var id = 3 * col + row + 1;
 
-
-    var id1 = 3 * y + x + 1;
-    var id2 = 3 * (y - 1) + (x - 1) + 1;
-    var id3 = 3 * y + (x - 1) + 1;
-    var id4 = 3 * (y + 1) + (x - 1) + 1;
-    var id5 = 3 * (y - 1) + x + 1;
-    var id6 = 3 * (y + 1) + x + 1;
-    var id7 = 3 * (y - 1) + (x + 1) + 1;
-    var id8 = 3 * y + (x + 1) + 1;
-    var id9 = 3 * (y + 1) + (x + 1) + 1;
-
-    var tab = [id1, id2, id3, id4, id5, id6, id7, id8, id9];
-
-    for (i = 0; i < 9; i++) {
-        if (tab[i] > 0 && tab[i] < 10) { tab1.push(tab[i]); }
-
+        if (id > 0 && id < 10) { f1 = document.getElementById("id" + id).innerHTML };
+        else { f1 = "empty" };
     }
 
-    console.log(tab);
-    console.log(tab1);
+    var check2 = function(x, y) {
+        var row = x - 1;
+        var col = y;
+        var id = 3 * col + row + 1;
 
-
-    for (i = 0; i < tab1.length; i++) {
-        console.log(tab1[i]);
-        var f = document.getElementById("id" + tab1[i]).innerHTML;
-        console.log(f);
-
+        if (id > 0 && id < 10) { f2 = document.getElementById("id" + id).innerHTML };
+        else { f2 = "empty" };
     }
 
+    var check3 = function(x, y) {
+        var row = x - 1;
+        var col = y + 1;
+        var id = 3 * col + row + 1;
+
+        if (id > 0 && id < 10) { f3 = document.getElementById("id" + id).innerHTML };
+        else { f3 = "empty" };
+    }
+
+    var check4 = function(x, y) {
+        var row = x;
+        var col = y - 1;
+        var id = 3 * col + row + 1;
+
+        if (id > 0 && id < 10) { f4 = document.getElementById("id" + id).innerHTML };
+        else { f4 = "empty" };
+    }
+
+    var check5 = function(x, y) {
+        var row = x;
+        var col = y;
+        var id = 3 * col + row + 1;
+
+        if (id > 0 && id < 10) { f5 = document.getElementById("id" + id).innerHTML };
+        else { f5 = "empty" };
+    }
+
+    var check6 = function(x, y) {
+        var row = x;
+        var col = y + 1;
+        var id = 3 * col + row + 1;
+
+        if (id > 0 && id < 10) { f6 = document.getElementById("id" + id).innerHTML };
+        else { f6 = "empty" };
+    }
+
+    var check7 = function(x, y) {
+        var row = x + 1;
+        var col = y - 1;
+        var id = 3 * col + row + 1;
+
+        if (id > 0 && id < 10) { f7 = document.getElementById("id" + id).innerHTML };
+        else { f7 = "empty" };
+    }
+
+    var check8 = function(x, y) {
+        var row = x + 1;
+        var col = y;
+        var id = 3 * col + row + 1;
+
+        if (id > 0 && id < 10) { f8 = document.getElementById("id" + id).innerHTML };
+        else { f8 = "empty" };
+    }
+
+    var check9 = function(x, y) {
+        var row = x + 1;
+        var col = y + 1;
+        var id = 3 * col + row + 1;
+
+        if (id > 0 && id < 10) { f9 = document.getElementById("id" + id).innerHTML };
+        else { f9 = "empty" };
+    }
+
+    check1(x, y);
+    check2(x, y);
+    check3(x, y);
+    check4(x, y);
+    check5(x, y);
+    check6(x, y);
+    check7(x, y);
+    check8(x, y);
+    check9(x, y);
+
+    console.log(f1);
+    console.log(f2);
+    console.log(f3);
+    console.log(f4);
+    console.log(f5);
+    console.log(f6);
+    console.log(f7);
+    console.log(f8);
+    console.log(f9);
+
+
+
+
+
+    var win = function(p1, p2, p3) {
+        if (p1 === p2 && p1 === p3) {
+            p1.style.background = "green";
+            p2.style.background = "green";
+            p3.style.background = "green";
+        }
+    }
+
+
+    win(f1, f2, f3);
+    win(f4, f5, f6);
+    win(f7, f8, f9);
+    win(f1, f4, f7);
+    win(f2, f5, f8);
+    win(f3, f6, f9);
+    win(f1, f5, f9);
+    win(f7, f5, f3);
 
 }
