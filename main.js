1fd46e2334d8e5d5ce37774d43d3a58571cb9a60
learneeer <learneeer.9@gmail.com>
2019-06-11 21:15:27 +0200

    gra_dziala

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 var mark = "x";
 
 
-var check = function(row, col) {
-    var nr = 3 * row + col + 1;
+var check = function(col, row) {
+    var nr = 3 * col + row + 1;
     var p = document.getElementById("id" + nr);
 
     if (p.innerHTML === "") {
         if (mark === "x") { mark = "o" } else { mark = "x" }
         p.innerHTML = mark;
+    }
+
+    for (i = 0; i < tab.length; i++) {
+        spr(tab[i]);
     }
 }
 
@@ -54,39 +58,29 @@ var tab = [
     ]
 ]
 
-var cos2 = function(c) {
-    var c0 = c[0];
-    var c1 = c[1];
-    var nr = 3 * c0 + c1 + 1;
+
+var spr = function(c) {
+    var p1_y = c[0][0];
+    var p1_x = c[0][1];
+
+    var p2_y = c[1][0];
+    var p2_x = c[1][1];
+
+    var p3_y = c[2][0];
+    var p3_x = c[2][1];
+
+    var p1 = document.getElementById("id" + (3 * p1_y + p1_x + 1));
+    var p2 = document.getElementById("id" + (3 * p2_y + p2_x + 1));
+    var p3 = document.getElementById("id" + (3 * p3_y + p3_x + 1));
 
 
-    console.log(c0);
-    console.log(c1);
-    console.log(nr);
-    console.log(p);
 
-
-}
-
-var cos = function(w) {
-    var p0 = w[0];
-    var p1 = w[1];
-    var p2 = w[2];
-
-    console.log(p0);
-    console.log(p1);
-    console.log(p2);
-
-    cos2(p0);
-    cos2(p1);
-    cos2(p2);
-
-
+    if (p1.innerHTML !== "" && p2.innerHTML !== "" && p3.innerHTML !== "") {
+        if (p1.innerHTML === p2.innerHTML && p1.innerHTML === p3.innerHTML) {
+            p1.style.background = "green";
+            p2.style.background = "green";
+            p3.style.background = "green";
+        }
+    }
 
 }
-
-for (i = 0; i < tab.length; i++) {
-    console.log(tab[i]);
-}
-
-cos(tab[0]);
